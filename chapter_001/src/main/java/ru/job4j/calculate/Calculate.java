f98b5ef864ec042898e9e52d2156c555d92d3fa3
SemenProbochkin <sakr487331@gmail.com>
2019-02-10 00:33:36 +0300

    1.5. Checkstyle[#109362]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,14 +2,14 @@ package ru.job4j.calculate;
  /**
  * Calculate.
  *
- *@author Probochkin.Semen
+ *@author Probochkin.Semen.
  */
-public class Calculate{
+public class Calculate {
 	/**
 	* Main
 	* @param args - arg.
 	*/
-	public static void main(String[] args){
+	public static void main(String[] args) {
 System.out.println("We will not use 'Hello, World'");
 }
 
