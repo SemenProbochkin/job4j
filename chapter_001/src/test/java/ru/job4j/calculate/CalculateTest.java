bc5008c735dc5c46dd26faa299d4857f11979f8c
SemenProbochkin <sakr487331@gmail.com>
2019-02-09 23:06:53 +0300

    1.5. Checkstyle[#109362]

---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Semen Probochkin (semion.probochkins@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenThreeEchoPlusName() {
+    String input = "Semen Probockins";
+    String expect = "Echo, echo, echo : Semen Probockins"; 
+    Calculate calc = new Calculate();
+    String result = calc.echo(input);
+    assertThat(result, is(expect));
+}
+ 
+}
---
+++ chapter_001/src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculatetest task.
+*
+* @author Probochkin Semen (semion.probochkin@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
