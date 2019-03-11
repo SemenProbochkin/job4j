f2708b45236b071b45f49fe1231cb256198e3e1a
SemenProbochkin <sakr487331@gmail.com>
2019-03-12 00:08:12 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#109331]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+/**
+ * Counter.
+ *
+ * @author Probochkin Semion.
+ * @version $Id$.
+ * @since 0.1
+ */
+
+public class Counter {
+    /**
+     * Add - суммирование четных чисел в диапозоне.
+     * @param start - первый аргумент.
+     * @param finish - второй аргумент.
+     * @return sum - сумма чисел.
+     */
+    public int add(int start, int finish) {
+        int sum = 0;
+        for(int index = start ;index <= finish;index++ ){
+            if (index % 2 == 0) {
+                sum = sum + index;
+            }
+        }
+        return sum;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+/**
+ * CounterTest.
+ *
+ * @author Probochkin Semion.
+ * @version $Id$.
+ * @since 0.1
+ */
+public class CounterTest {
+
+    /**
+     * Test add.
+     */
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter check = new Counter();
+        int result = check.add(1,12);
+        assertThat(result, is(30));
+    }
+}
