9962d559dbca0ce75b8ae76ea5cbcd8c521ddf64
SemenProbochkin <sakr487331@gmail.com>
2020-03-24 21:58:07 +0300

    5.5. Рефакторинг кода.

--- chapter_001/src/main/java/ru/job4j/loop/Board.java
+++ chapter_001/src/main/java/ru/job4j/loop/Board.java
@@ -4,12 +4,12 @@ public class Board {
       public String paint(int width, int height) {
        StringBuilder screen = new StringBuilder();
         String ln = System.lineSeparator();
-              for ( int y = 1; y <= height; y++) {
-        for (int x= 1; x <= width; x++ ) {
+              for (int y = 1; y <= height; y++) {
+        for (int x = 1; x <= width; x++) {
 
                 // условие проверки, что писать пробел или X
                 // Выше в задании мы определили закономерность, когда нужно проставлять X
-                if ((x+y)%2==0) {
+                if ((x + y) % 2 == 0) {
                     screen.append("X");
                 } else {
                     screen.append(" ");
